bd1b82d77d7eb610975eb3b23761ded4bcfb4bc6
VictorSmoke <joaovictorcarvalho086@gmail.com>
2025-07-07 00:00:15 -0300

    Iniciando projeto.

---
+++ Projeto Sao_Jorge_Suplementos/core/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+def base(request):
+    return render(request, 'base.html')
+
+def cabecalho(request):
+    return render(request, 'cabecalho.html')
+
+def rodape(request):
+    return render(request, 'rodape.html')
+
+def main(request):
+    return render(request, 'main.html')
+
