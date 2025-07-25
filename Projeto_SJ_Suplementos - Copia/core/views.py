f9e66c5a19e5d78b4562d7848b41717adb28f96b
VictorSmoke <joaovictorcarvalho086@gmail.com>
2025-07-24 22:34:42 -0300

    Mudanças.

---
+++ Projeto_SJ_Suplementos - Copia/core/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+def base(request):
+    return render(request, 'base.html')
+
+def cabecalho(request):
+    return render(request, 'cabecalho.html')
+
+def footer(request):
+    return render(request, 'footer.html')
+
+def main(request):
+    return render(request, 'main.html')
+
