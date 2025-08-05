7566f9e17dcb79d1bac4f06f7cf7698ca45c9f35
Onogan11 <gabrielsilvahefiel@gmail.com>
2025-08-05 15:59:15 -0300

    Adicionado a biblioteca React ao Projeto

--- Projeto_SJ_Suplementos/Sao_Jorge_Suplementos/settings.py
+++ Projeto_SJ_Suplementos/Sao_Jorge_Suplementos/settings.py
@@ -16,6 +16,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+]
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'Sao_Jorge_Suplementos.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': []
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
