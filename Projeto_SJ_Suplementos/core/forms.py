95e65f7625253a117257983fe9a86ecf10d204a6
João Victor de Carvalho <162042777+JoaoVictorCarvalho89@users.noreply.github.com>
2025-10-09 13:25:06 -0300

    comentários.

--- Projeto_SJ_Suplementos/core/forms.py
+++ Projeto_SJ_Suplementos/core/forms.py
@@ -5,3 +5,13 @@ class AreaForm(forms.ModelForm):
     class Meta:
         model = Area
         fields = ['nome']
+        
+"""class InstrutorForm(forms.ModelForm):
+    class Meta:
+        model = Instrutor
+        fields = ['nome']
+        
+class PublicoForm(forms.ModelForm):
+    class Meta:
+        model = Publico
+        fields = ['nome']"""
